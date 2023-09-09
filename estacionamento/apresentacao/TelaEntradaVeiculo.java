2db8e03b3dd7f0d7773ed6017a147f7374e5b102
Edgar Jesus <mendesbr92@gmail.com>
2023-09-09 14:47:56 -0300

    Implementado a Tela Inicial Movimentacao

--- estacionamento/apresentacao/TelaEntradaVeiculo.java
+++ estacionamento/apresentacao/TelaEntradaVeiculo.java
@@ -4,5 +4,12 @@ import javax.swing.*;
 
 public class TelaEntradaVeiculo extends JFrame{
 	
+	private JFrame parent;
+
+	public TelaEntradaVeiculo(JFrame parent) {
+		// TODO Auto-generated constructor stub
+		this.parent = parent;
+	}
+	
 
 }
